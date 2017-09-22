239397cc155c89240d615cbe178b55eac846ff8a
Aydar Farrakhov <aydar.farrakhov@dz.ru>
2017-09-22 11:58:56 +0300

    auth service: use auth table instead memory

---
+++ auth-service/src/main/groovy/com/example/security/config/OAuth2Config.groovy
@@ -0,0 +1,88 @@
+package com.example.security.config
+
+import com.example.security.user.UserSecurityDetailsService
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.beans.factory.annotation.Qualifier
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.context.annotation.Primary
+import org.springframework.security.authentication.AuthenticationManager
+import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer
+import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer
+import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer
+import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerSecurityConfigurer
+import org.springframework.security.oauth2.provider.token.DefaultTokenServices
+import org.springframework.security.oauth2.provider.token.TokenStore
+import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenConverter
+import org.springframework.security.oauth2.provider.token.store.JwtTokenStore
+
+/**
+ * @author Aydar Farrakhov 
+ * @since 20.09.2017
+ */
+@EnableAuthorizationServer
+@Configuration
+class OAuth2Config extends AuthorizationServerConfigurerAdapter {
+
+
+    @Autowired
+    @Qualifier("authenticationManagerBean")
+    private AuthenticationManager authenticationManager
+
+    @Autowired
+    private UserSecurityDetailsService userDetailsService
+
+    @Override
+    void configure(ClientDetailsServiceConfigurer clients) throws Exception {
+        clients.inMemory()
+                .withClient("app")
+                .secret("secret")
+                .authorizedGrantTypes("refresh_token", "password", "client_credentials")
+                .scopes("webclient", "mobileclient")
+                .and()
+                .withClient("server")
+                .secret("secret")
+                .authorizedGrantTypes("refresh_token", "password", "client_credentials")
+                .scopes("server")
+    }
+
+
+    @Override
+    void configure(AuthorizationServerEndpointsConfigurer endpoints) throws Exception {
+        endpoints
+                .tokenStore(tokenStore())
+                .accessTokenConverter(accessTokenConverter())
+                .authenticationManager(authenticationManager)
+                .userDetailsService(userDetailsService)
+    }
+
+    @Override
+    void configure(AuthorizationServerSecurityConfigurer oauthServer) throws Exception {
+        oauthServer
+                .tokenKeyAccess("permitAll()")
+                .checkTokenAccess("isAuthenticated()")
+    }
+
+    @Bean
+    TokenStore tokenStore() {
+        return new JwtTokenStore(accessTokenConverter())
+    }
+
+    @Bean
+    JwtAccessTokenConverter accessTokenConverter() {
+        JwtAccessTokenConverter converter = new JwtAccessTokenConverter()
+        converter.setSigningKey("123")
+        return converter
+    }
+
+    @Bean
+    @Primary
+    DefaultTokenServices tokenServices() {
+        DefaultTokenServices defaultTokenServices = new DefaultTokenServices()
+        defaultTokenServices.setTokenStore(tokenStore())
+        defaultTokenServices.setSupportRefreshToken(true)
+        return defaultTokenServices
+    }
+
+}
